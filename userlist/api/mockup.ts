1841811aebda5683ffb662f852b910b7b69ac837
Gustavo <gmonte@tutanota.com>
2022-11-22 18:33:21 -0800

    add mockup data

---
+++ userlist/api/mockup.ts
@@ -0,0 +1,38 @@
+export const data = [
+  {
+    id: 1,
+    first_name: 'Fiann',
+    last_name: 'Gronauer',
+    email: 'fgronauer0@diigo.com',
+  },
+  {
+    id: 2,
+    first_name: 'Mona',
+    last_name: 'McNally',
+    email: 'mmcnally1@earthlink.net',
+  },
+  {
+    id: 3,
+    first_name: 'Gaylord',
+    last_name: 'Bernetti',
+    email: 'gbernetti2@instagram.com',
+  },
+  {
+    id: 4,
+    first_name: 'Tybie',
+    last_name: 'Rudolph',
+    email: 'trudolph3@ucla.edu',
+  },
+  {
+    id: 5,
+    first_name: 'Mel',
+    last_name: 'Springle',
+    email: 'mspringle4@mapquest.com',
+  },
+  {
+    id: 6,
+    first_name: 'Gideon',
+    last_name: 'Korous',
+    email: 'gkorous5@hhs.gov',
+  },
+];
